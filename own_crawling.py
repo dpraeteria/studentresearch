39f128fd6f24bcc0ca77e8f11d6406cdc3ccf36c
praeteria <60338811+dpraeteria@users.noreply.github.com>
2023-09-07 15:13:29 +0900

    다음 뉴스로 전환, 크롤링 완성, 코드 최적화
    목적에 맞게 코드 최적화함(리스트 하나로 통합), 다음 뉴스로 전환하고, 다음 뉴스의 경제 탭에서 크롤링함
    
    ┏중요한 정보
    ┠find_links(날짜, 페이지) 넣으면 링크 크롤링
    ┠이부분은 혹시 된다면 페이지 수도 찾아서 알맞게 수정할 예정
    ┗보완할 부분 : 날짜 받아서 자동화 되게 해야함

--- own_crawling.py
+++ own_crawling.py
@@ -5,37 +5,41 @@ from openpyxl import Workbook
 import pandas as pd
 from openpyxl.utils.dataframe import dataframe_to_rows
 from tqdm import tqdm
+from datetime import date, datetime
 
-def url_parsing(page , date):
-    url = f"https://www.google.com/search?q=경제+뉴스&tbm=nws&tbs=cdr:1,cd_min:{date},cd_max:{date}&start={50*(page-1)}"
+
+start_time = int(input('Enter Start Time : '))
+end_time = int(input('Enter End Time : '))
+
+
+# def format_date(input_date):
+#     formatted_date = input_date.strftime("%Y%m%d")
+#     return formatted_date
+
+article_list = []
+
+def url_parsing(date,page):
+    url = f"https://news.daum.net/breakingnews/economic?regDate={date}&page={page}"
     return url
 
-req_url = requests.get(url_parsing(1,"8/30/2023"), headers={"User-Agent": "Mozilla/5.0"\
+
+def find_links(date,page):
+    global article_list
+    req_url = requests.get(url_parsing(date,page), headers={"User-Agent": "Mozilla/5.0"\
     "(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "\
     "Chrome/116.0.5845.96 Safari/537.36"})
+    soup = BeautifulSoup(req_url.text, "lxml")
 
-soup = BeautifulSoup(req_url.text, "lxml")
-def find_a():
-    # a_tag = soup.find_all("a")
-    tag_lst = []
-    for i in tqdm(range(50)):
-        article_element = soup.select_one(f'div.SoaBEf > div > div > a')
-        # article['']
-        if article_element != None:
-            tag_lst.append(article_element['href'])
-        else:
-            continue
-    return tag_lst
-    
-#################################DEEP 한 요소 추출################################
-#rso > div > div > div:nth-child(1) > div > div > a > div > div.iRPxbe > div.n0jPhd.ynAwRc.MBeuO.nDgy9d
-#rso > div > div > div:nth-child(2) > div > div > a > div > div.iRPxbe > div.n0jPhd.ynAwRc.MBeuO.nDgy9d
-##마지막 : 
+    for i in tqdm(range(0,15)):
+        article = soup.select_one(f"#mArticle > div.box_etc > ul > li:nth-child({i+1}) > div > strong > a")
+        article_href = article['href']
+        article_list.append(article_href)
 
-################################링크 추출########################################
-#rso > div > div > div:nth-child(1) > div > div > a
+for i in range(0,4):
+    find_links("20230906",i+1)
 
-# url = url_parsing(1,"8/30/2023")
 
-print(find_a())
-print(f'\n\n 길이 : {len(find_a())}')
+
+
+print(list(set(article_list)))
+print(len(article_list))
