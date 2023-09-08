c25b33414d19171b108c33ce73e726514b54b255
ccoopcnn12338 <ccoopcnn12383@gmail.com>
2023-09-08 22:03:35 +0900

    마지막 페이지 찾는거 최적화

--- crawling_modules.py
+++ crawling_modules.py
@@ -14,10 +14,11 @@ def check_page(date : int, i : int):
     return None
 
 def find_last_page(date : int):
-    req_url = requests.get(url_parsing(20230904,1000), headers=header)
+    req_url = requests.get(url_parsing(date,1000), headers=header)
     soup = BeautifulSoup(req_url.text, "lxml")
     return int(soup.select(f"#mArticle > div.box_etc > div.paging_news > span.inner_paging > a.num_page")[-1].contents[0])+1
 #특정 날짜의 마지막 기사 페이지 찾는거 최적화
+#만약 300페이지 까지 밖에 없는데 1000페이지를 요청하면 자동으로 맨 마지막 페이지로 다이렉트됨을 이용함
 
 
 ##링크에서 기사를 추출하는 함수
@@ -38,6 +39,3 @@ def flatten_list(lst : list) -> list:
         else:
             result.append(item)
     return result
-
-
-print(0)
